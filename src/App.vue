6fd42453f3450e8814601a5f161ee0aa0fb5ded9
Nikita <nikita.proger21@gmail.com>
2021-08-17 16:16:11 +0300

    Stop rerender page with using event.preventDefault()

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,8 @@ export default {
     };
   },
   methods: {
-    createPost() {
+    createPost(event) {
+      event.preventDefault();
       const newPost = {
         id: Date.now(),
         title: this.title,
