f6ec29e631e9cbe2a8443c28da8828c9204032de
Nikita <nikita.proger21@gmail.com>
2021-08-17 14:16:34 +0300

    Simple counter was created with using methods field, v-on directive and @ for adding listener 'click'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
+// Component MarkUp
 <template>
   <div>
+      <button v-on:click="addlike">Like</button>
+      <button @click="addDislike">Dislike</button>
     <div>likes: <strong>{{ likes }}</strong></div>
+    <div>dislikes: <strong>{{ dislikes }}</strong></div>
   </div>
 </template>
 
 <script>
+//Component model
 export default {
   data() {
     return {
       likes: 0,
+      dislikes: 0
     };
   },
+  methods: {
+      addlike(){
+          this.likes += 1
+      },
+      addDislike(){
+          this.dislikes += 1
+      } 
+  }
 };
 </script>
 
